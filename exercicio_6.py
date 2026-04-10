896a6dd38bd72ced9f0918e48dd107e8864e2819
Wagner Lucas <96394353+Followsz@users.noreply.github.com>
2026-04-10 02:33:27 -0300

    Add files via upload

--- exercicio_6.py
+++ exercicio_6.py
@@ -3,15 +3,15 @@ pais = input("Para qual país você vai viajar?: \n")
 if pais == "Estados Unidos":
     reais = float(input("Quantos reais você quer converter para dólares? \n"))
     dolares = reais / 5.0  
-    print(f"Você terá {dolares:.2f} dólares.")
+    print(f"{dolares:.2f} USD.")
 
 elif pais == "Argentina":
     reais = float(input("Quantos reais você quer converter para pesos argentinos? \n"))
     pesos = reais * 180
-    print(f"Você terá {pesos:.2f} pesos argentinos.")
+    print(f"{pesos:.2f} ARS.")
 
 elif pais == "Japão":
     reais = float(input("Quantos reais você quer converter para ienes? \n"))
     ienes = reais * 30 
-    print(f"Você terá {ienes:.2f} ienes.")
+    print(f"{ienes:.2f} JPY.")
 else:    print("Não temos a moeda disponível para esse país.") 
