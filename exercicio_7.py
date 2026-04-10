e6b9e969a600b4616db9d5362b4c8ce47e348d02
Wagner Lucas <96394353+Followsz@users.noreply.github.com>
2026-04-10 02:38:10 -0300

    Add files via upload

--- exercicio_7.py
+++ exercicio_7.py
@@ -1,20 +1,16 @@
-frequencia = float(input("Digite a frequência populacional: \n"))
-gene = input("Digite o gene: \n").upper()
-impacto = input("Digite o impacto: \n").upper()
-reads = int(input("Digite o número de reads: \n"))
-vaf = float(input("Digite o VAF: \n"))
+frequencia_populacional = float(input("Digite a frequencia: "))
+gene = input("Digite o gene: ")
+impacto = input("Digite o impacto (ALTO ou BAIXO): ")
+reads = int(input("Digite os reads: "))
+vaf = float(input("Digite o VAF: "))
 
-if reads < 10 or vaf < 20:
-    print("Não relevante")
+genes_de_excecao = (gene == 'HFE') or (gene == 'MEFV') or (gene == 'GJB2')
 
-elif impacto == "ALTO":
-    print("É relevante")
-
-elif frequencia > 5 and gene not in ["HFE", "MEFV", "GJB2"]:
-    print("Não é relevante")    
-
-elif vaf > 0 and vaf < 100: 
-    print("É relevante")
-
+if (reads < 10) or (vaf < 20):
+    print("Não é relevante, deve ser artefato.")
+elif impacto != "ALTO":
+    print("Não é relevante.")
+elif (frequencia_populacional > 5) and not genes_de_excecao:
+    print("Não é relevante.")
 else:
-    print("É relevante")
+    print("É relevante.")
